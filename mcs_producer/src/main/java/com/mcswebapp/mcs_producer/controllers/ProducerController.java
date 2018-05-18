3051f0125f182a5cf0eef47c6bba3bb7a2b462ca
Niranjan <niranjanamurthy@gmail.com>
2018-05-19 06:44:51 +1000

    added producer controller

---
+++ mcs_producer/src/main/java/com/mcswebapp/mcs_producer/controllers/ProducerController.java
@@ -0,0 +1,13 @@
+package com.mcswebapp.mcs_producer.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProducerController {
+
+	@RequestMapping("/")
+	public String testController() {
+		return "Its working fine";
+	}
+}
