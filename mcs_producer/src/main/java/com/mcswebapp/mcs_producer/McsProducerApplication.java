eec5b429eef812d03529fc7a10c21b410a71402e
Niranjan <niranjanamurthy@gmail.com>
2018-05-19 06:25:41 +1000

    First commit

---
+++ mcs_producer/src/main/java/com/mcswebapp/mcs_producer/McsProducerApplication.java
@@ -0,0 +1,12 @@
+package com.mcswebapp.mcs_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McsProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McsProducerApplication.class, args);
+	}
+}
