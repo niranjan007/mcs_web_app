e394d1d4e1d329c1171a7f9e0a94cca564808b12
Niranjan <niranjanamurthy@gmail.com>
2018-05-19 10:24:21 +1000

    War settings

--- mcs_producer/src/main/java/com/mcswebapp/mcs_producer/McsProducerApplication.java
+++ mcs_producer/src/main/java/com/mcswebapp/mcs_producer/McsProducerApplication.java
@@ -2,11 +2,19 @@ package com.mcswebapp.mcs_producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class McsProducerApplication {
+public class McsProducerApplication extends SpringBootServletInitializer	  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(McsProducerApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(McsProducerApplication.class);
 	}
 }
